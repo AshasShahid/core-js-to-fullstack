b75bdd3832c3197e299d920082911c5f59213fc4
AshasShahid <104735008+AshasShahid@users.noreply.github.com>
2026-07-03 08:28:33 +0000

    datatypes

---
+++ 01-core-javascript/02-datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all code as newer version of js
+// alert(3 + 3) it will work in the browser but not in nodejs
+console.log(3 + 4); // code readability matters it read sum of two numbers
+let name = "Ashas Shahid";
+let age = 27;
+let isLoggedIn = true;
+let state; // variable is declared but value is not defined
+
+// number => 2 power of 53
+// bigInt 
+// string => ""
+// boolean => true/false
+// null => satndalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof age) // type: number
+console.log(typeof null) // type: object
+console.log(typeof undefined) // type: undefined
