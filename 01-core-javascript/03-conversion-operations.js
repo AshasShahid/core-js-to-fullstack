41596a098da0bacac54821b5fbb5909d633fc9e5
AshasShahid <104735008+AshasShahid@users.noreply.github.com>
2026-07-04 11:43:22 +0000

    conversion datatypes

---
+++ 01-core-javascript/03-conversion-operations.js
@@ -0,0 +1,29 @@
+let score = '33';
+let score2 = '33abc'; // string can contain other characters
+let score3 = null;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let ValueInNumber = Number(score);
+console.log(typeof(ValueInNumber));
+
+let ValueInNumber2 = Number(score2);
+console.log(typeof(ValueInNumber2));
+console.log(ValueInNumber2); // will return NaN not a number 
+
+let ValueInNumber3 = Number(score3);
+console.log(typeof(ValueInNumber3));
+console.log(ValueInNumber3); // it will return zero
+
+// the same way undefined will return NaN and boolean value true will return 1 and 0 for false
+// some more conversions 
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+// if we try an empty string "" it will return false 
+// if we use any string "Ashas" it will return true
+let someNumber = 27;
+let stringNumber = String(someNumber);
+console.log(stringNumber); // it will return a string 27
+console.log(typeof(stringNumber)); 
